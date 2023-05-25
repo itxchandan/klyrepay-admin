a53e15aa61e66b2bc01b6eac7f3d4aaa9c6bda59
VikasRana007666 <cookthewebpage@gmail.com>
2023-05-26 00:02:23 +0530

    version 2.0.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,52 @@
 // New York Time
 window.onload = displayClock();
-function displayClock(){
-  const currentDate = new Date(new Date().toLocaleString("en-US", {timeZone: "America/New_York"}));
-  var timestamp = currentDate.toString().split('GMT');
+function displayClock() {
+  const currentDate = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "America/New_York" })
+  );
+  var timestamp = currentDate.toString().split("GMT");
   var splitstring = timestamp[0].split(" ");
-  var final_string = splitstring[0]+', '+splitstring[1]+' '+splitstring[2]+' '+splitstring[3]+', '+splitstring[4];
+  var final_string =
+    splitstring[0] +
+    ", " +
+    splitstring[1] +
+    " " +
+    splitstring[2] +
+    " " +
+    splitstring[3] +
+    ", " +
+    splitstring[4];
   $("#clock").text(final_string);
-  setTimeout(displayClock, 1000); 
+  setTimeout(displayClock, 1000);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   // Theme Mode
-  $(".theme-mode").click(function(){
-     var currentSrc = $('html').attr('data-bs-theme');
-      if (currentSrc === 'light') {
-        $('html').attr('data-bs-theme', 'dark');
-        $('.theme-sun').addClass("d-none");
-        $('.theme-moon').removeClass("d-none");
-      } else {
-        $('html').attr('data-bs-theme', 'light');
-        $('.theme-sun').removeClass("d-none");
-        $('.theme-moon').addClass("d-none");
-      }
+  $(".theme-mode").click(function () {
+    var currentSrc = $("html").attr("data-bs-theme");
+    if (currentSrc === "light") {
+      $("html").attr("data-bs-theme", "dark");
+      $(".theme-sun").addClass("d-none");
+      $(".theme-moon").removeClass("d-none");
+    } else {
+      $("html").attr("data-bs-theme", "light");
+      $(".theme-sun").removeClass("d-none");
+      $(".theme-moon").addClass("d-none");
+    }
+  });
+
+  // Toggle Password
+  $(".eye-toggle-button").click(function (e) {
+    e.preventDefault();
+    var eyeToggleButton = $(this);
+    var input = eyeToggleButton.siblings("input");
+
+    eyeToggleButton.find("i").toggleClass("hidden");
+
+    if (eyeToggleButton.find("i").first().hasClass("hidden")) {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
   });
 });
