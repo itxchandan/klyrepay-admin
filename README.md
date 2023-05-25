0ac4a279d32626b63d66624c00e4ae3e7a9f1321
VikasRana007666 <cookthewebpage@gmail.com>
2023-05-25 22:18:54 +0530

    version 2.0.3

---
+++ README.md
@@ -0,0 +1,4 @@
+## Resources
+* https://elstar.themenate.net/ui-components/skeleton
+* Bootstrap Version: 5.3.0-alpha3
+* Sass Compiler: sass --watch assets/scss/bootstrap.scss assets/css/bootstrap.css
---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// New York Time
+window.onload = displayClock();
+function displayClock(){
+  const currentDate = new Date(new Date().toLocaleString("en-US", {timeZone: "America/New_York"}));
+  var timestamp = currentDate.toString().split('GMT');
+  var splitstring = timestamp[0].split(" ");
+  var final_string = splitstring[0]+', '+splitstring[1]+' '+splitstring[2]+' '+splitstring[3]+', '+splitstring[4];
+  $("#clock").text(final_string);
+  setTimeout(displayClock, 1000); 
+}
+
+$(document).ready(function(){
+  // Theme Mode
+  $(".theme-mode").click(function(){
+     var currentSrc = $('html').attr('data-bs-theme');
+      if (currentSrc === 'light') {
+        $('html').attr('data-bs-theme', 'dark');
+        $('.theme-sun').addClass("d-none");
+        $('.theme-moon').removeClass("d-none");
+      } else {
+        $('html').attr('data-bs-theme', 'light');
+        $('.theme-sun').removeClass("d-none");
+        $('.theme-moon').addClass("d-none");
+      }
+  });
+});
